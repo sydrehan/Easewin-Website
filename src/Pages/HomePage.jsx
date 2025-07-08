48c927b3d815b84b80ec19df4c6d982ae5055586
Rehan Raza <sydrehanraza@gmail.com>
2025-07-08 14:42:06 +0530

    new

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,12 +5,14 @@ import Hero from '../components/Hero';
 import Features from '../components/Features';
 import Testimonials from '../components/Testimonials';
 import CTA from '../components/CTA';
+import About from '../components/About';
 
 const HomePage = () => {
   return (
     // NO Navbar or Footer here! They are now in the Layout component.
     <>
       <Hero />
+      <About />
       <Features />
       <Testimonials />
       <CTA />
